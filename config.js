9b799e3d1111f5977c1d0a3e9043a35942954c7b
pierre <piercus@gmail.com>
2012-06-15 11:59:58 +0200

    add initial server and config file

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {},
+    config.potraceCmd = SHELL_POTRACE_CMD,
+    config.publicFolder = PATH_FOR_PUBLIC_FOLDER,
+    config.publicUrl = URL_FOR_PUBLIC_FOLDER,
+    config.port = 3080;
+exports = config;
---
+++ jpg2svg.js
@@ -0,0 +1,39 @@
+var config = require('./config'),
+    app = require("express").createServer(),
+    util  = require('util'),
+    exec = require('child_process').exec,
+    md5 = require('MD5'),
+    fs = require("fs"); // the second arg is the command 
+                                          // options
+
+app.get("/",function(req, res){
+  var imgUrl = req.query["img"],
+      imgName = md5(imgUrl)+".svg",
+      child = exec('wget -qO- '+imgUrl+' | djpeg -bmp | '+config.potraceCmd+' -s',
+    function(error, stdout, stderr){
+      if (error !== null) {
+        console.log('exec error: ' + error);
+        res.send({
+          error : error
+        });
+      } else {
+        console.log("stdout : "+stdout);
+        fs.writeFile(config.publicFolder+imgName,stdout,function(err){
+          //res.writeHead(200, ['Content-Type', 'json/plain']);
+          if(err){
+            console.log(err);
+            res.send({
+              error : err
+            });
+          } else {
+            res.send({
+              url : config.publicUrl+imgName
+            });
+          }
+        });
+      }
+  })
+  
+});
+
+app.listen(config.port);
