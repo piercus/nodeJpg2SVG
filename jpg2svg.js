103aba526a50bf04731ec37f0e850035bad78925
pierre <piercus@gmail.com>
2012-06-17 16:57:32 +0200

    colors

--- jpg2svg.js
+++ jpg2svg.js
@@ -1,38 +1,155 @@
 var config = require('./config'),
+    mergeSVGFiles = require('./mergeSVGFiles'),
+    libxmljs = require("libxmljs"),
     app = require("express").createServer(),
     util  = require('util'),
     exec = require('child_process').exec,
     md5 = require('MD5'),
-    fs = require("fs"); // the second arg is the command 
-                                          // options
+    fs = require("fs"),
+    //debug fjs-style
+    d = false; 
+
 
 app.get("/",function(req, res){
   var imgUrl = req.query["img"],
+      ncolors = req.query["ncolors"]|| 1,
       imgName = md5(imgUrl)+".svg",
-      child = exec('wget -qO- '+imgUrl+' | djpeg -bmp | '+config.potraceCmd+' -s',
-    function(error, stdout, stderr){
-      if (error !== null) {
-        console.log('exec error: ' + error);
-        res.send({
-          error : error
-        });
-      } else {
-        fs.writeFile(config.publicFolder+"/"+imgName,stdout,function(err){
-          //res.writeHead(200, ['Content-Type', 'json/plain']);
-          if(err){
-            console.log(err);
-            res.send({
-              error : err
+      d=this.debug;
+  
+  if(!ncolors){
+    
+    // black & white
+    
+    var child = exec('wget -qO- '+imgUrl+' | djpeg -bmp | '+config.potraceCmd+' -s',
+      function(error, stdout, stderr){
+        if (error !== null) {
+          console.log('exec error: ' + error);
+          res.send({
+            error : error
+          });
+        } else {
+          fs.writeFile(config.publicFolder+"/"+imgName,stdout,function(err){
+            //res.writeHead(200, ['Content-Type', 'json/plain']);
+            if(err){
+              console.log(err);
+              res.send({
+                error : err
+              });
+            } else {
+              console.log("SVG Successfuly created "+imgName);
+              res.send({
+                url : config.publicUrl+"/"+imgName
+              });
+            }
+          });
+        }
+    })
+  } else {
+    var n = parseInt(ncolors);
+    if(isNaN(n)){
+      console.log("Bad argument ncolors");
+      res.send({
+        error : "Bad Argument ncolors"
+      });
+      return;
+    }
+
+    var tmp = "./tmp/",
+        tmpImg = tmp+"img.pnm",
+        tmpMapFile = tmp+"mapFile.pnm",
+        hist = tmp+"hist",
+        simpleImg = tmp+"simg.pnm";
+        
+    var cmds = [
+        'rm -rf '+tmp+'*',
+        'wget -qO- '+imgUrl+' | jpegtopnm > '+tmpImg,
+        'pnmcolormap '+(n+1)+' '+tmpImg+'>'+tmpMapFile,
+        'ppmhist -hexcolor -colorname -noheader '+tmpMapFile+' >'+hist,
+        'pnmremap -mapfile '+tmpMapFile+' '+tmpImg+'>'+simpleImg
+    ];
+    exec(cmds.join(";"),
+          function(error, stdout, stderr){
+            console.log("stdout : ",stdout);
+            
+            colors = [];
+            fs.readFile(hist,"UTF-8",function(err,data){
+              if (error !== null) {
+                console.log('exec error: ' + error);
+                res.send({
+                  error : error
+                });
+              } else {
+                d&&console.log("data",data);
+                
+                // get hexa colors
+                var cs = data.toString().match(/00([0-9a-f]{2})/g),
+                    hex,
+                    colors = [],
+                    tmpColorFiles = [],
+                    tmpColorCmd = [];
+                d&&console.log('cs: ' + cs.toString());
+                
+                
+                // remove the lightest color
+                var maxColorStrength = 0,
+                    color;
+                for(var i = 0; i < (n+1); i++){
+                  var strength = (parseInt(cs[i*3].slice(2),16) + parseInt(cs[i*3].slice(2),16) + parseInt(cs[i*3].slice(2),16)),
+                      hex = "#"+cs[i*3].slice(2)+cs[i*3+1].slice(2)+cs[i*3+2].slice(2);
+                  if(strength<maxColorStrength){
+                    colors.push(hex)
+                  } else {
+                    color&&colors.push(color); 
+                    maxColorStrength = strength;
+                    color = hex;
+                  }
+                }
+                
+                for(var i = 0; i < n; i++){
+                  //tmpMapColorFiles.push(tmp+i+"mapfilecolor.pnm");
+                  tmpColorFiles.push(tmp+i+"color.svg");
+                  tmpColorCmd.push(
+                    'ppmcolormask -color='+colors[i]+' '+simpleImg+'>'+tmp+i+'.pnm; ppmtojpeg '+tmp+i+'.pnm | djpeg -bmp | '+config.potraceCmd+' -s > '+tmpColorFiles[i]);
+                }
+                d&&console.log("cmd",tmpColorCmd.join(";"));
+                
+                exec(tmpColorCmd.join(";"),function(error, stdout, stderr){
+                  d&&console.log("stdout",stdout,error,stderr);
+                  mergeSVGFiles(tmpColorFiles,colors,function(error,svg){
+                    if (error !== null) {
+                      console.log('exec error: ' + error);
+                      res.send({
+                        error : error
+                      });
+                    } else {
+                      //console.log(svg.toString()); 
+                      fs.writeFile(
+                        config.publicFolder+"/"+imgName,
+                        svg.toString(),
+                        function(err,data){
+                          
+                          if (error !== null) {
+                            console.log('exec error: ' + error);
+                            res.send({
+                              error : error
+                            });
+                          } else {
+                            res.send({
+                              url : config.publicUrl+"/"+imgName
+                            });
+                          }
+                      }); 
+                    }
+                  });
+                  
+                });
+                
+                
+              }
             });
-          } else {
-            console.log("SVG Successfuly created "+imgName);
-            res.send({
-              url : config.publicUrl+"/"+imgName
-            });
-          }
         });
-      }
-  })
+  }
+
   
 });
 app.listen(config.port);
---
+++ mergeSVGFiles.js
@@ -0,0 +1,59 @@
+var fs = require("fs"),
+    libxmljs = require("libxmljs"); 
+
+
+module.exports = function(files,colors,cb){
+  var n = colors.length;
+  var paths = {},gs={};
+  
+  var mergeGroups = function(gs,w,h){
+    console.log("bf merge")
+    
+    var doc = new libxmljs.Document("1.0", "UTF-8");
+    var svg = doc.node("svg");
+        svg.attr({
+          version:'1.0',
+          xmlns: 'http://www.w3.org/2000/svg',
+          width: w+"pt",
+          height : h+"pt",
+          viewBox:"0 0 "+w+" "+h
+        });
+    
+    for(var i in gs) if(gs.hasOwnProperty(i)){
+      svg.addChild(gs[i]);      
+    }
+    cb(null,doc);
+  }
+  var maxh = 400, maxw = 400;
+  
+  for(var i = 0; i < files.length; i++){
+    fs.readFile(files[i],"UTF-8",function(){
+      var c = colors[i];
+      return function(err,data){
+        if(err) return cb(err);
+        console.log("data toString()",data.toString());
+        var xmlDoc = libxmljs.parseXmlString(data);
+        console.log("c",c);
+        paths[c] = xmlDoc.find('//path');
+        var g = xmlDoc.childNodes()[3];
+        var w = xmlDoc.root().attr("width").value(),
+            h = xmlDoc.root().attr("height").value();
+        
+        for(var p = 0; p < g.childNodes().length; p++){
+          var pa = g.childNodes()[p];
+          if(pa.name()!== "text"){
+            console.log("bf attr",pa.attrs(),pa.name());
+            pa.attr({
+              fill : c
+            });
+            console.log("bf attr");
+          }
+        }
+        (w>maxw)&& (maxw=w);
+        (h>maxh)&& (maxh=h);
+        g&&(gs[c] = g); 
+        (!--n)&&mergeGroups(gs,maxh,maxw);
+      };
+    }());
+  }
+}
